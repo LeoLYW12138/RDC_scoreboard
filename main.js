e4c669359f059600bf6afc504afe81a503f91f08
LeoLYW12138 <leo62227@gmail.com>
2021-12-21 02:56:46 +0800

    lots of changes

--- main.js
+++ main.js
@@ -10,15 +10,15 @@ const records = {
   blue: [],
 };
 const team_names = [
-  "Team 1",
-  "Team 2",
-  "Team 3",
-  "Team 4",
-  "Team 5",
-  "Team 6",
-  "Team 7",
-  "Team 8",
-  "Team 9",
+  "Alkaline Metal",
+  "Now",
+  "Delta Three-ta",
+  "4gotton",
+  "Team FIVE",
+  "Cancer",
+  "Group 7",
+  "Infinity",
+  "Cloud9",
 ];
 
 const btn_start = document.querySelector("#start");
@@ -26,12 +26,16 @@ const btn_stop = document.querySelector("#stop");
 const btn_reset = document.querySelector("#reset");
 const btn_save = document.querySelector("#save");
 const btn_load = document.querySelector("#load");
+const btn_final = document.querySelector("#cal-final-score");
 const down_timer = document.querySelector("#countdown-timer");
 const up_timer = document.querySelector("#countup-timer");
+const selects = document.querySelectorAll(".team-name");
 
 const score_boards = {
   red: document.querySelector("#red-scoreboard"),
   blue: document.querySelector("#blue-scoreboard"),
+  redBig: document.querySelector("#red-score"),
+  blueBig: document.querySelector("#blue-score"),
 };
 
 const THREE_MIN = 3 * 60 * 1000;
@@ -39,6 +43,16 @@ let countdown_time = 0;
 let countup_time = 0;
 let countdownId = 0;
 let countupId = 0;
+let gv = null;
+
+selects.forEach((select) => {
+  team_names.forEach((name) => {
+    const option = document.createElement("option");
+    option.value = name;
+    option.text = name;
+    select.appendChild(option);
+  });
+});
 
 counters.forEach((counter) => {
   counter_states[counter.id] = 0;
@@ -49,6 +63,7 @@ counters.forEach((counter) => {
     const team = counter.id.split("-").slice(0, 1);
     const record = new Record(countup_time, counter.id, score[team]);
     score_boards[team].prepend(record.toHTML());
+    score_boards[team + "Big"].innerText = score[team].score;
 
     records[team].push(record.toObj());
   });
@@ -76,6 +91,13 @@ function resetAll() {
 
   score_boards.red.innerHTML = "";
   score_boards.blue.innerHTML = "";
+  score_boards.redBig.innerText = "0";
+  score_boards.blueBig.innerText = "0";
+
+  if (gv) {
+    gv.outerHTML = "";
+    gv = null;
+  }
 }
 
 function countdown() {
@@ -84,6 +106,7 @@ function countdown() {
   if (countdown_time <= 0) {
     clearInterval(countdownId);
     remaining = "00:00:00";
+    down_timer.classList.add("times-up");
   } else {
     remaining = msToString(countdown_time);
   }
@@ -96,6 +119,7 @@ function countup() {
   if (countup_time >= THREE_MIN) {
     clearInterval(countupId);
     time = msToString(THREE_MIN);
+    up_timer.classList.add("times-up");
   } else {
     time = msToString(countup_time);
   }
@@ -135,7 +159,7 @@ btn_reset.addEventListener("click", () => {
 btn_save.addEventListener("click", () => {
   const final = evalResult(counter_states, true);
   saveResult(
-    { red: team_names[1], blue: team_names[0] },
+    { red: selects[1].value, blue: selects[0].value },
     {
       red: {
         final: final.red.score,
@@ -152,6 +176,29 @@ btn_save.addEventListener("click", () => {
   );
 });
 
+btn_final.addEventListener("click", () => {
+  const final = evalResult(counter_states, true);
+  score_boards.blueBig.innerText = +final.blue.score.toFixed(1);
+  score_boards.redBig.innerText = +final.red.score.toFixed(1);
+});
+
 btn_load.addEventListener("click", () => {});
 
-document.addEventListener("greatVictory", (e) => {});
+document.addEventListener("greatVictory", (e) => {
+  if (!gv) {
+    clearInterval(countdownId);
+    clearInterval(countupId);
+    btn_stop.click();
+
+    const div = document.createElement("div");
+    div.classList.add("great-victory");
+    const h1 = document.createElement("h1");
+    h1.innerText = "Great Victory";
+    h1.classList.add("text-size-3xl", "font-bold");
+    div.appendChild(h1);
+
+    const board = score_boards[e.detail.team + "Big"];
+    board.parentNode.insertBefore(div, board.nextSibling);
+    gv = div;
+  }
+});
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import WindiCSS from "vite-plugin-windicss";
 
 export default {
+  server: {
+    port: 3030,
+    strictPort: true,
+  },
   plugins: [WindiCSS({ config: "./windi.config.js" })],
 };
