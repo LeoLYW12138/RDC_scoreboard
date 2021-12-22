70bd3e25e67b292ee267b01886ec8b647fa98c17
LeoLYW12138 <leo62227@gmail.com>
2021-12-22 16:18:43 +0800

    modify deploy options

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ export default {
     port: 3030,
     strictPort: true,
   },
+  base: "/RDC2021/",
+  build: {
+    outDir: "dist/RDC2021",
+  },
   plugins: [WindiCSS({ config: "./windi.config.js" })],
 };
