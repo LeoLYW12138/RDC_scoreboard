d6a60466eceac83011783656b31675319dea1328
LeoLYW12138 <leo62227@gmail.com>
2022-12-14 19:02:21 +0800

    deploy

--- 2022/deploy.sh
+++ 2022/deploy.sh
@@ -3,24 +3,12 @@
 # abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
-cd dist
+cp -r ./dist/** ../gh-page/
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-# git init
 git add -A
-git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:HKUST-Robotics-Team/HKUST-Robotics-Team.github.io.git main
-git push -u origin main
+git commit -m "deploy"
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+git push
